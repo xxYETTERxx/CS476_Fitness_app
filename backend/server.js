361deaaff5bdcda2144ad39223a4f2fc13bdef18
Nathan Ottenson <ottenson@uregina.ca>
2024-03-03 21:45:12 -0600

    Updating JSON file to fix dependcy issue

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/users');
 const app = express();
 const cors = require('cors');
+
 app.use(cors());
+app.use(express.json()); //parse JSON bodies
 
 const port = process.env.PORT || 5000;
 app.listen(port,() => {
     console.log('Server is running on port:' + port );
 })
 
-app.use(express.json()); //parse JSON bodies
 
 app.use('/api/auth', authRoutes);
 
