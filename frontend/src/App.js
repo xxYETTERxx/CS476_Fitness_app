97abd9d622a70e9e0a47dd8fa91ad59091e65206
Tariq Lashley <tariqlash@gmail.com>
2024-03-07 15:24:47 -0600

    adding tailwind functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import SignUp from './components/SignUp';
 
-function App(){
+function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <SignUp />
     </div>
   );
