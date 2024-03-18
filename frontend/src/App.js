b315071c99a161dc9978619e5a5934e4e6e42b6b
Tariq Lashley <tariqlash@gmail.com>
2024-03-18 09:30:04 -0600

    Routers added for multiple page setup and styling added for create account page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,33 +4,59 @@ import SignIn from './components/SignIn';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 
-function App(){
+function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  
-  useEffect (() => {
+
+  useEffect(() => {
     const token = localStorage.getItem('token');
     console.log('Token from storage: ', token);
-    if (token){
+    if (token) {
       setIsAuthenticated(true);
     }
   }, [])
   return (
-    <div className="App bg-base-100">
-      <Navbar/>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      {/* <Hero/> */}
-      {isAuthenticated && <Dashboard/>};
-      <div style = {{marginLeft: 20}}>
-        <SignUp />
-        <br/>
-        <SignIn setIsAuthenticated={setIsAuthenticated}/>
+
+
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={
+    //       <>
+    //         <Hero />
+    //   />
+    // </Routes>
+
+    <Router>
+      <div className="App bg-base-200">
+        <Navbar />
+
+        <Routes>
+          <Route
+            path='/tester'
+            element={
+              <>
+                <Hero/>
+              </>
+            }
+          
+          />
+        </Routes>
+
+        {/* <Hero/> */}
+        {isAuthenticated && <Dashboard />}
+        <div>
+          <SignUp />
+          <br />
+          <SignIn setIsAuthenticated={setIsAuthenticated} />
+        </div>
       </div>
-    </div>
+    </Router>
+
+
   );
 }
 
---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,212 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import logo from '../images/gg11.png'
+
+
+function SignUp() {
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userType, setUserType] = useState('basic');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            // construct user data
+            const userData = {
+                userName,
+                email,
+                password,
+                userType
+            };
+
+            //Send backend request
+            const response = await axios.post('http://localhost:5000/api/auth/register', userData)
+
+            if (response.status === 200 || response.status === 201) {
+                alert('User registered successfully!');
+
+                setUserName('');
+                setEmail('');
+                setPassword('');
+            }
+
+
+
+
+        } catch (error) {
+            if (error.message.includes('ERR_CONNECTION_REFUSED') || error.message.includes('Network Error')) {
+                alert('Connection to server failed');
+            } else if (error.response) {
+                console.error("Registration failed:", error.response.data);
+                alert("Registration failed: " + error.response.data);
+            }
+            else {
+                console.error("Registration failed:", error.message);
+                alert("Registration failed: ", error.message);
+            }
+        }
+    };
+
+
+    return (
+        <div className=''>
+
+            <form onSubmit={handleSubmit} className='w-full flex justify-center'>
+
+            {/* <div className='w-full flex justify-center'> */}
+               
+                
+                <div class="w-5/6 max-w-2xl mt-5 md:mt-16">
+                <div>
+                    <h2 class="text-base font-semibold leading-7 text-base-content">Create Account</h2>
+                    <p class="mt-1 text-sm leading-6 text-gray-600">Create an account to manage meals and workouts</p>
+                </div>
+                <div className='flex flex-col md:flex-row items-center justify-between'>
+                    <div class="m-3">
+                        <div class="mt-5 flex justify-center items-center gap-x-3">
+                            <svg class="h-32 w-32  text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                            </svg>
+                            <button type="button" class="rounded-md bg-base px-2.5 py-1.5 text-sm font-semibold text-base-content shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
+                        </div>
+                        </div>
+
+                        <div class="sm:col-span-4 m-3 ">
+                            <label for="username" class="block text-sm font-medium leading-6 text-base-content mr-5">Account Type:</label>
+
+                            <div class="mt-2 flex justify-center items-center"
+                            value={userType}
+                            onChange={(e) => setUserType(e.target.value)}
+                            required>
+                                <input type="radio" value='basic' name='accountType' aria-label="Basic" className="btn input-bordered md:btn-lg bg-base-100  w-24 mr-5" />
+                                <input type="radio" value='pro' name='accountType' aria-label="Pro" className="btn input-bordered md:btn-lg bg-base-100  w-24" />
+                            </div>
+                        </div>
+                    </div>
+                    
+                {/* <input type="text" placeholder="Type here" className="input input-bordered w-full" /> */}
+                    <div className='flex pl-3 pr-3'>
+                        <div class="sm:col-span-4 w-full mr-3">
+                            <label for="username" class="block text-sm font-medium leading-6 text-base-content">First Name:</label>
+                            <div class="mt-2">
+                                <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-4 w-full">
+                            <label for="username" class="block text-sm font-medium leading-6 text-base-content">Last Name:</label>
+                            <div class="mt-2">
+                                <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="m-3">
+                        <label class="block text-sm font-medium leading-6 text-base-content">Username:</label>
+                        <div class="mt-2 ">
+                            <input
+                                type="username"
+                                placeholder="Type here"
+                                className="input input-bordered w-full"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                                required
+                            />
+                        </div>
+                    </div>                    
+                    
+                    
+                    <div class="m-3">
+                        <label class="block text-sm font-medium leading-6 text-base-content">Email:</label>
+                        <div class="mt-2 ">
+                            <input
+                                type="email"
+                                placeholder="Type here"
+                                className="input input-bordered w-full"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                    </div>   
+
+                    <div class="m-3">
+                        <label class="block text-sm font-medium leading-6 text-base-content">Password:</label>
+                        <div class="mt-2 ">
+                            <input
+                                type="password"
+                                placeholder="Type here"
+                                className="input input-bordered w-full"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                    </div> 
+
+                    <div className='divider mt-10 mb-3 pr-3 pl-3'></div>
+                    <div class="mt- mr-3 flex items-center justify-end gap-x-6">
+                        <button type="button" class="text-sm font-semibold leading-6 text-base-content">Cancel</button>
+                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Account</button>
+                    </div>
+
+                
+                </div>
+            </form>
+
+
+
+
+
+
+
+            {/* <h2><b>Create Account</b></h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>UserName:</label>
+                    <input
+                        type="username"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Account Type:</label>
+                    <select
+                        value={userType}
+                        onChange={(e) => setUserType(e.target.value)}
+                        required
+                    >
+                        <option value='basic'>Basic</option>
+                        <option value='pro'>Pro</option>
+                    </select>
+                </div>
+                <button type="sumbit">Create Account</button>
+            </form> */}
+        </div>
+
+    );
+}
+
+export default SignUp;
