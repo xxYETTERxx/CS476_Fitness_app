c8d7de719f165cf436cd59623212bf62fc8fbf1c
Tariq Lashley <tariqlash@gmail.com>
2024-03-25 13:33:49 -0400

    dashboard tweaks and Day-Month-Year toggle

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -123,23 +123,21 @@ const Dashboard = ({setNutritionActive}) => {
     }
 
     return (
-        <div className='flex justify-center pt-5 pb-5'>
-            <div className="card card-side shadow-xl flex flex-col
-                bg-base-300 pl-4 pr-4 justify-between max-w-xl">
-                <div className='flex justify-between text-2xl font-medium b h-1/6
-                    items-center'>
-                    <h2>{username}</h2>
+        <div className='flex flex-col items-center justify-center h-screen p-5'>
+            <div className="card card-side shadow-xl flex flex-col bg-base-300 p-4 pt-0 max-w-xl " >
+                <div className='flex justify-between text-2xl font-medium h-1/6 items-center'>
+                    <h2 className='mt-1'>{username}</h2>
                
                 </div>
-                <div className='flex h-3/6  w-full'>
-                    <div className='flex flex-col w-2/6 items-center justify-center '>
+                <div className='flex h-4/6 w-full'>
+                    <div className='flex flex-col w-2/6 items-center justify-around'>
                         {
                             avatar ?
-                            <img src={avatar} className='w-5/6 rounded-lg border border-neutral mb-3' />:
-                            <img src={blankUser} className='w-5/6 rounded-lg border border-neutral mb-3' />
+                            <img src={avatar} className='w-5/6 rounded-lg border border-neutral' />:
+                            <img src={blankUser} className='w-5/6 rounded-lg border border-neutral' />
                         }
-                        <div className='flex h-1/6 items-center'>
-                            <text className='text-xl font-bold text-success pr-2'><span>-</span>2<span className='text-lg text-base-content'>lbs</span>
+                        <div className='flex items-center'>
+                            <text className='text-xl font-bold text-success mr-2'><span>-</span>2<span className='text-lg text-base-content'>lbs</span>
                             </text>
                             <button className="btn btn-square scaleButton">
                                 <div className="indicator">
@@ -149,51 +147,58 @@ const Dashboard = ({setNutritionActive}) => {
                             </button>
                         </div>
                     </div>
-                    <div className='flex flex-col w-4/6 pl-3 pr-3'>
-                      
-                        <button className="btn btn-outline btn-neutral w-full mb-4">Exercise</button>
-                        <button onClick = {nActive} className="btn btn-outline btn-neutral w-full mb-4">Food</button>
+                    <div className='flex flex-col w-4/6 p-3 justify-center'>
+
+                        <button className="btn btn-outline btn-neutral w-full">Exercise</button>
+                        <button onClick={nActive} className="btn btn-outline btn-neutral w-full mb-3 mt-3">Food</button>
+                        
                         {
-                            userType === 'pro' &&(
-                            <button className="btn btn-outline btn-neutral w-full">Workout Planner</button>
-                        )}
+                            userType === 'pro' && (
+                                <button className="btn btn-outline btn-neutral w-full">Workout Planner</button>
+                            )}
 
                     </div>
 
 
                 </div>
-                <div className='flex flex-col  h-2/6 pt-8'>
-                    <div className='flex justify-between pb-3'>
-                        <div className='flex flex-col w-full pl-2'>
+                <div className='flex flex-col h-1/6 items-center w-full ml-5'>
+                    <div className='flex mb-3 w-full' >
+                        <div className='flex flex-col w-full '>
                             <div>
                                 <text className='text-xl font-semibold'><WaterIntakeComponent /></text>
                             </div>
                             <caption className='text-sm flex'>WATER INTAKE</caption>
                         </div>
                         <div className='divider'>|</div>
-                        <div className='flex flex-col w-full pl-2'>
+                        <div className='flex flex-col w-full '>
                             <div className='flex'>
                                 <text className='w-2/3 text-xl font-semibold'><CalorieIntakeComponent /></text>
                                 <text className='text-xl font-semibold'>-</text>
                             </div>
                             <caption className='text-sm flex'>CAL IN</caption>
                         </div>
-                        <div className='flex flex-col w-full pl-2'>
-                        <div className='flex'>
-                        <text className='w-2/3 text-xl font-semibold'><CalorieBurnComponent /></text>
+                        <div className='flex flex-col w-full '>
+                            <div className='flex'>
+                                <text className='w-2/3 text-xl font-semibold'><CalorieBurnComponent /></text>
                                 <text className='text-xl font-semibold'>=</text>
                             </div>
                             <caption className='text-sm flex'>CAL BURN</caption>
                         </div>
-                        <div className='flex flex-col w-full pl-2'>
+                        <div className='flex flex-col w-full '>
                             <div>
-                            <text className='text-xl font-semibold'><NetCalorieComponent /></text>
+                                <text className='text-xl font-semibold'><NetCalorieComponent /></text>
+                                <caption className='text-sm flex'>TOTAL</caption>
                             </div>
-                            
+
                         </div>
                     </div>
                 </div>
 
+            </div>
+            <div className='flex justify-center mt-3 w-3/6'>
+                <input type="radio" value='day' name='dateToggle' aria-label="Day" className="btn input-bordered bg-base-100  w-24" checked/>
+                <input type="radio" value='month' name='dateToggle' aria-label="Month" className="btn input-bordered bg-base-100  w-24 ml-5 mr-5" />
+                <input type="radio" value='year' name='dateToggle' aria-label="Year" className="btn input-bordered bg-base-100  w-24" />
             </div>
         </div>
 
