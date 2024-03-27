131a78f7f1899b58850842f8e5cf803799e53d6c
Nathan Ottenson <ottenson@uregina.ca>
2024-03-27 00:00:26 -0600

    updating mongoDB url address

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,17 @@ app.use(cors());
 app.use(express.json()); //parse JSON bodies
 
 const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
+.then (() => console.log ('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
+app.use('/api/auth', authRoutes);
+
 app.listen(port,() => {
     console.log('Server is running on port:' + port );
 })
 
 
-app.use('/api/auth', authRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/fitnessAppDB')
 
-.then (() => console.log ('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err));
